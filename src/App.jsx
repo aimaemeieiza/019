fd22bca4c39e73b96255a985b7d89a0b1f95c46d
Nattawoot <heartjaja45@gmail.com>
2024-03-23 01:13:06 +0700

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -10,47 +10,40 @@ function App() {
   return (
     <main id="main">
       <div className="leftPort">
-        <ImageProfile image="/src/assets/react.svg" />
+        <ImageProfile image="/src/assets/A019.jpeg" />
+        <Title title="ประวัติส่วนตัว">
 
-        <Title title="Thanadet Kaewsri (Matt)">
-          <h3>ตำแหน่ง : Frontend</h3>
+          <p>ชื่อ เจษฏาพร ขุนเณร</p>
+          <p>ชื่อเล่น อิ่ม</p>
+          <p>อายุ 20 ปี</p>
+          <p>วันเกิด {moment("2003/05/06").format("D MMM YYYY")}</p>
         </Title>
 
-        <Title title="Contact">
-     
-          <p>วันที่ปัจจุบัน: {moment().format("D MMM YYYY")}</p>
-          <p>วันเกิด: {moment("1994/07/01").format("D MMM YYYY")}</p>
-          <p style={{ display: hide ? 'none': 'block'}} >
-            Tel : +66877543002
-          </p>
-          <button onClick={() => setHide(!hide)}>
-            {hide ? 'Show': 'Hide'}
-          </button>
-          <p>Email : mattssru@gmail.com</p>
-          <p>www.mattssru@gmail.com</p>
-        </Title>
-    
-        <Title title="Education">
-          <p>ปริญญาตรี สาขาเทคโนโลยีสารสนเทศ</p>
-          <p>มหาวิทยาลัยราชภัฏสวนสุนันทา</p> 
+        <Title title="ช่องทางติดต่อ">
+          <p>TEL  088-749-7133</p>
+          <p>Line aimkhunnen</p> 
+          <p>Facebook Aim</p> 
+          <p>Instagram a.jsdp</p> 
+          <p>E-mail Jetsadapornza43@gmail.com</p> 
         </Title>
       </div>
 
       <div className="rightPort">
-        <Title title="Profile">
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit quas natus soluta debitis nihil placeat, molestiae quae distinctio. Corporis dolores sit similique vitae dignissimos rerum saepe dolorem laboriosam rem impedit.</p>
+      <Title title="การศึกษา">
+          <p>กำลังศึกษา ปริญญาตรีชั้นปีที่ 3 ที่ มหาวิทยาลัยราชภัฎสวนสุนนันทา คณะวิทยาศาสตร์และเทคโนโลยี สาขาเทคโนโลยีสารสนเทศ </p>
         </Title>
 
-        <Title title="Work Experience">
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid veritatis eligendi cumque, laborum dolore error at, excepturi eum distinctio iste facere obcaecati natus impedit eaque eveniet nulla? Cupiditate, blanditiis in?</p>
+        <Title title="เกรดเฉลี่ย">
+          <p>GPA เกรดเฉลี่ยสะสม 3.75</p>
         </Title>
 
-        <Title title="Skills">
-          <p>HTML / CSS / JS</p>
-          <p>React.js</p>
-          <p>Next.js</p>
-          <p>Vue.js</p>
-          <p>Git</p>
+        <Title title="ประสบการณ์และคุณสมบัติ">
+          <p> 1.ทำโปรเจคเรื่องการศึกษาและพัฒนาเว็บเพื่อให้ความรู้
+ เกี่ยวกับการออกแบบที่ถูกต้องและการศึกษาการเกิด
+ ก๊าซเรือนกระจกจากการใช้ไฟฟ้า</p>
+          <p>2.เว็บไซต์ให้ความรู้การออกแบบเว็บไซต์และสามารถ
+ คำนวณก๊าซเรือนกระจกจากการใช้ไฟฟ้า</p>
+
         </Title>
       </div>
     </main>
